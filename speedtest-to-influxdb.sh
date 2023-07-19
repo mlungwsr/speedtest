b9b69dd47dcc1ac3121af16df278e1173041f081
Raphael Sabelo Mlungwana <ralph.mlu@gmail.com>
2023-07-19 19:52:24 +0000

    Added container names and renamed dockerfiles

--- speedtest-to-influxdb.sh
+++ speedtest-to-influxdb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run speedtest-cli and capture the JSON output
-json_output=$(speedtest-cli --json)
+#json_output=$(speedtest-cli --json)
+json_output='{"download": 9939293.721194586, "upload": 2486555.260160513, "ping": 42.484, "server": {"url": "http://jnb1.wiocc.net:8080/speedtest/upload.php", "lat": "-26.2044", "lon": "28.0456", "name": "Johannesburg", "country": "South Africa", "cc": "ZA", "sponsor": "WIOCC", "id": "12109", "host": "jnb1.wiocc.net:8080", "d": 28.566047531870637, "latency": 42.484}, "timestamp": "2023-07-19T01:45:58.502684Z", "bytes_sent": 4030464, "bytes_received": 14119132, "share": null, "client": {"ip": "129.232.219.207", "lat": "-26.3811", "lon": "27.8376", "isp": "xneelo", "isprating": "3.7", "rating": "0", "ispdlavg": "0", "ispulavg": "0", "loggedin": "0", "country": "ZA"}}'
 
 # Extract the required fields from the JSON output (modify this based on your JSON structure)
 download_speed=$(echo $json_output | jq -r '.download')
